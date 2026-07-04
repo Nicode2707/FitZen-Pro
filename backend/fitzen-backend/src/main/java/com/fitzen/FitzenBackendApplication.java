d5fe443d0b38b13cd041b5280034f0dfa414dfcc
Niraj Kumar <nirajkr2707@gmail.com>
2026-07-04 13:23:05 +0530

    Refactor backend structure for deployment

--- backend/fitzen-backend/src/main/java/com/fitzen/FitzenBackendApplication.java
+++ backend/fitzen-backend/src/main/java/com/fitzen/FitzenBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FitzenBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FitzenBackendApplication.class, args);
 	}
 
